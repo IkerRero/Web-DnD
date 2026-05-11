423eaa6cd550546c614a53584977f214ce0557dc
IkerRero <ikerreromartinez@gmail.com>
2026-05-11 14:34:16 +0200

    Map now locks movement also in mobile

--- ScriptMap.js
+++ ScriptMap.js
@@ -1,17 +1,28 @@
 const map = document.querySelector(".map");
 
-map.addEventListener("mouseenter", () => {
-    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+function lockScroll() {
+    const scrollbarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
+
     if (scrollbarWidth > 0) {
         document.body.style.paddingRight = `${scrollbarWidth}px`;
     }
+
     document.body.style.overflow = "hidden";
     document.documentElement.style.overflow = "hidden";
-});
+}
 
-map.addEventListener("mouseleave", () => {
+function unlockScroll() {
     document.body.style.overflow = "";
     document.documentElement.style.overflow = "";
     document.body.style.paddingRight = "";
-});
+}
 
+// Desktop
+map.addEventListener("mouseenter", lockScroll);
+map.addEventListener("mouseleave", unlockScroll);
+
+// Mobile
+map.addEventListener("touchstart", lockScroll, { passive: true });
+map.addEventListener("touchend", unlockScroll);
+map.addEventListener("touchcancel", unlockScroll);
