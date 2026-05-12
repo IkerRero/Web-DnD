230462bcb3dc3dbe3ac1e38ddb05c065537f695c
Jake19094 <sergiolasbit@gmail.com>
2026-05-12 06:20:20 +0200

    Página Contacto
    Los divs son responsive solo que falta agregar mis imágenes, texto de relleno y arreglar el problema de que el botón volver del nav desaparezca en esta página.

---
+++ ScriptContacto.js
@@ -0,0 +1,20 @@
+        function toggleAutor(integrantes) {
+            const img = integrantes.querySelector('img');
+            const titulo = integrantes.querySelector('h3');
+            const parrafo = integrantes.querySelector('p');
+
+            // Comprobar si estamos en el estado 1
+            const esEstado1 = img.getAttribute('src') === img.getAttribute('data-img1');
+
+            if (esEstado1) {
+                // Cambiar a estado 2
+                img.src = img.getAttribute('data-img2');
+                titulo.innerText = titulo.getAttribute('data-t2');
+                parrafo.innerText = parrafo.getAttribute('data-p2');
+            } else {
+                // Volver a estado 1
+                img.src = img.getAttribute('data-img1');
+                titulo.innerText = titulo.getAttribute('data-t1');
+                parrafo.innerText = parrafo.getAttribute('data-p1');
+            }
+        }
